f04950be25fa00aa1f3fed9b8ced1ad85746b9da
Pascal <rost@informatik.uni-hamburg.de>
2019-12-05 11:45:52 +0100

    Added simple python script to load project data from csv to solr

---
+++ scripts/README.txt
@@ -0,0 +1,8 @@
+How to use:
+
+Put CSV file with the data to be transfered to Solr in the same folder as the script.
+
+pip3 install pysolr
+python3 csv_to_solr.py
+
+Uses a solr core with the name "ubo_projects"
---
+++ scripts/csv_to_solr.py
@@ -0,0 +1,29 @@
+import csv
+import pysolr
+
+def read_csv():
+    project_data = []
+    with open('191121_EU-Projekte.csv') as csv_file:
+        reader = csv.reader(csv_file, delimiter=';')
+        for line, row in enumerate(reader):
+            if line == 0:
+                print(row)
+            else:
+                data = {}
+                data['projekt_id'] = row[0]
+                data['acronym'] = row[1]
+                data['projekttitel'] = row[2]
+                data['kostentraeger'] = row[3]
+                data['foerderkennzeichen'] = row[4]
+                project_data.append(data)
+    return project_data
+
+def update_solr(project_data_list):
+    solr = pysolr.Solr('http://localhost:8081/solr/ubo_projects')
+    solr.add(project_data_list)
+    solr.commit()
+        
+
+if __name__ == '__main__':
+    project_data = read_csv()
+    update_solr(project_data)
