8ee8ce29cbc6bfcf700e254987ba90cbfbc939ea
Possommi <Possommi@users.noreply.github.com>
2024-05-03 17:41:47 +0200

    #462 Enable genre mapping mapping to/from orcid (#463)
    * 462 Added x-mapping to entries in ubogenre.xml
    
    * #462 Added MCR.EventHandler.MCRObject.016.Class=org.mycore.mods.classification.MCRClassificationMappingEventHandler
    
    * #462 Added orcidWorkType.xml
    
    * #462 Added mcr2work.xsl and work2mcr.xsl
    
    * #462 Added mapping for encyclopedia-entry and dictionary-entry to ubogenre.xml
    
    * #462 Fixed x-mapping
    
    * #462 Added missing type of works
    
    * #462 Set MCR.EventHandler.MCRObject.018A.Class   = org.mycore.orcid2.v3.work.MCRORCIDWorkEventHandlerImpl
    
    * #462 Fixed id
    
    * #462 Map remaining types
    
    * #462 Enable orcid event handler in Jena only

--- common/src/main/java/de/uni_jena/thunibib/user/ThUniBibUtils.java
+++ common/src/main/java/de/uni_jena/thunibib/user/ThUniBibUtils.java
@@ -5,17 +5,27 @@ import jakarta.persistence.criteria.CriteriaBuilder;
 import jakarta.persistence.criteria.CriteriaQuery;
 import jakarta.persistence.criteria.Root;
 import jakarta.persistence.criteria.SetJoin;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.mycore.backend.jpa.MCREntityManagerProvider;
 import org.mycore.common.config.MCRConfiguration2;
+import org.mycore.datamodel.classifications2.MCRCategory;
+import org.mycore.datamodel.classifications2.MCRCategoryDAO;
+import org.mycore.datamodel.classifications2.MCRCategoryDAOFactory;
+import org.mycore.datamodel.classifications2.MCRCategoryID;
+import org.mycore.datamodel.classifications2.MCRLabel;
 import org.mycore.user2.MCRUser;
 import org.mycore.user2.MCRUserAttribute;
 import org.mycore.user2.MCRUserAttribute_;
 import org.mycore.user2.MCRUser_;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
 public class ThUniBibUtils {
+
+    protected static final Logger LOGGER = LogManager.getLogger(ThUniBibUtils.class);
 
     protected static final String LEAD_ID_NAME = "id_" + MCRConfiguration2.getString("MCR.user2.matching.lead_id")
         .get();
@@ -52,5 +62,42 @@ public class ThUniBibUtils {
             .findFirst();
 
         return leadIdAttr.isEmpty() ? null : leadIdAttr.get().getValue();
+    }
+
+    /**
+     * Returns the x-mapping for the given mapping prefix.
+     * <br/>
+     * Example: <code>getXMapping("mir_genres", "article", "schemaOrg")</code> will return <code>ScholarlyArticle</code>
+     *
+     * @param classificationId
+     * @param categoryId
+     * @param mappingPrefix
+     *
+     * @return the mapping or an empty string when there is no such mapping for the given parameters
+     * */
+    public static String getXMapping(String classificationId, String categoryId, String mappingPrefix) {
+        MCRCategory category = null;
+        try {
+            MCRCategoryID categID = MCRCategoryID.fromString(classificationId + ":" + categoryId);
+            MCRCategoryDAO dao = MCRCategoryDAOFactory.getInstance();
+            category = dao.getCategory(categID, 0);
+            Optional<MCRLabel> label = category.getLabel("x-mapping");
+            if (label.isEmpty()) {
+                return "";
+            }
+            MCRLabel xmapping = label.get();
+            if (!xmapping.getText().contains(mappingPrefix)) {
+                return "";
+            }
+
+            Optional<String> result = Arrays.stream(xmapping.getText().split(" "))
+                .filter(mapping -> mapping.startsWith(mappingPrefix))
+                .findFirst();
+
+            return result.isPresent() ? result.get().split(":")[1] : "";
+        } catch (Throwable e) {
+            LOGGER.error("Could not load {}:{}", classificationId, categoryId, e);
+            return "";
+        }
     }
 }
