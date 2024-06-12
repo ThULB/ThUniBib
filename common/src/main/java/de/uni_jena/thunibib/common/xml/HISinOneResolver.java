1e3e04a4715e8a03b930d2960cb620c9620b098c
Silvio Hermann <s.hermann@uni-jena.de>
2024-06-12 09:41:05 +0200

    #550 Added template stub mycoreobject-mods-resolve-his-keys.xsl and defined transformer mods-resolve-his-keys

---
+++ common/src/main/java/de/uni_jena/thunibib/common/xml/HISinOneResolver.java
@@ -0,0 +1,39 @@
+package de.uni_jena.thunibib.common.xml;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.jdom2.Element;
+import org.jdom2.transform.JDOMSource;
+import org.mycore.common.xml.MCRFunctionResolver;
+
+import javax.xml.transform.Source;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.URIResolver;
+
+public class HISinOneResolver implements URIResolver {
+    public enum URI_PART {
+        language
+    }
+
+    private static Logger LOGGER = LogManager.getLogger(MCRFunctionResolver.class);
+
+    @Override
+    public Source resolve(String href, String base) throws TransformerException {
+        LOGGER.info("Resolving '{}'", href);
+
+        String[] parts = href.split(":");
+        String entity = parts[1];
+        String value = parts[2];
+
+        switch (URI_PART.valueOf(entity)) {
+            case language:
+                return new JDOMSource(new Element("int").setText(String.valueOf(resolveLanguage(value))));
+        }
+
+        throw new TransformerException("Unknown entity: " + entity);
+    }
+
+    private int resolveLanguage(String rfc5646) {
+        return 42;
+    }
+}
