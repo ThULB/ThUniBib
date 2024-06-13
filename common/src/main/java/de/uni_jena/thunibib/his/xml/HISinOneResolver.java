c8a3b0696400f1c5bee2c3c972cd66f71263ae1b
Silvio Hermann <s.hermann@uni-jena.de>
2024-06-13 13:40:17 +0200

    #550 Map ubo genre to his:publicationType (defaults to 'Sonstiger Publikationstyp' at the moment)

--- common/src/main/java/de/uni_jena/thunibib/his/xml/HISinOneResolver.java
+++ common/src/main/java/de/uni_jena/thunibib/his/xml/HISinOneResolver.java
@@ -3,22 +3,25 @@ package de.uni_jena.thunibib.his.xml;
 import de.uni_jena.thunibib.his.api.HISInOneClient;
 import de.uni_jena.thunibib.his.api.HISinOneClientFactory;
 import de.uni_jena.thunibib.his.api.v1.cs.sys.values.LanguageValue;
+import de.uni_jena.thunibib.his.api.v1.cs.sys.values.PublicationTypeValue;
 import jakarta.ws.rs.core.GenericType;
 import jakarta.ws.rs.core.Response;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.jdom2.Element;
 import org.jdom2.transform.JDOMSource;
+import org.mycore.common.xml.MCRXMLFunctions;
 
 import javax.xml.transform.Source;
 import javax.xml.transform.TransformerException;
 import javax.xml.transform.URIResolver;
 import java.util.List;
+import java.util.Optional;
 
 public class HISinOneResolver implements URIResolver {
 
     public enum SUPPORTED_URI_PARTS {
-        language
+        language, genre
     }
 
     private static final Logger LOGGER = LogManager.getLogger(HISinOneResolver.class);
@@ -34,9 +37,37 @@ public class HISinOneResolver implements URIResolver {
         switch (SUPPORTED_URI_PARTS.valueOf(entity)) {
             case language:
                 return new JDOMSource(new Element("int").setText(String.valueOf(resolveLanguage(value))));
+            case genre:
+                return new JDOMSource(new Element("int").setText(String.valueOf(resolveGenre(value))));
         }
 
         throw new TransformerException("Unknown entity: " + entity);
+    }
+
+    private int resolveGenre(String ubogenre) {
+        try (HISInOneClient hisClient = HISinOneClientFactory.create();
+            Response response = hisClient.get("cs/sys/values/publicationTypeValue")) {
+
+            List<PublicationTypeValue> pubTypeValues = response.readEntity(
+                new GenericType<List<PublicationTypeValue>>() {
+                });
+
+            Optional<PublicationTypeValue> tpv = pubTypeValues
+                .stream()
+                .filter(pubType -> pubType.getUniqueName().equals(MCRXMLFunctions.getDisplayName("ubogenre", ubogenre)))
+                .findFirst();
+
+            int id;
+            if (tpv.isEmpty()) {
+                id = pubTypeValues.stream()
+                    .filter(pubType -> "Sonstiger Publikationstyp".equals(pubType.getUniqueName()))
+                    .findFirst().get().getId();
+            } else {
+                id = tpv.get().getId();
+            }
+
+            return id;
+        }
     }
 
     private int resolveLanguage(String rfc5646) {
