3368d7ff62b1f006c24f8b965c2e4e2150d2994a
Silvio Hermann <s.hermann@uni-jena.de>
2024-06-13 14:21:09 +0200

    #550 Map publication state, introduced abstract CommonType

---
+++ common/src/main/java/de/uni_jena/thunibib/his/api/v1/CommonType.java
@@ -0,0 +1,70 @@
+package de.uni_jena.thunibib.his.api.v1;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+abstract public class CommonType {
+    @JsonProperty("id")
+    private int id;
+    @JsonProperty("lockVersion")
+    private int lockVersion;
+    @JsonProperty("objGuid")
+    private String objGuid;
+    @JsonProperty("shorttext")
+    private String shortText;
+    @JsonProperty("defaulttext")
+    private String defaultText;
+    @JsonProperty("longtext")
+    private String longText;
+    @JsonProperty("text")
+    private String text;
+    @JsonProperty("hiskeyId")
+    private int hisKeyId;
+    @JsonProperty("sortorder")
+    private int sortOrder;
+    @JsonProperty("uniquename")
+    private String uniqueName;
+
+    public int getId() {
+        return id;
+    }
+
+    public int getLockVersion() {
+        return lockVersion;
+    }
+
+    public String getObjGuid() {
+        return objGuid;
+    }
+
+    public String getShortText() {
+        return shortText;
+    }
+
+    public String getDefaultText() {
+        return defaultText;
+    }
+
+    public String getLongText() {
+        return longText;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public int getHisKeyId() {
+        return hisKeyId;
+    }
+
+    public int getSortOrder() {
+        return sortOrder;
+    }
+
+    public String getUniqueName() {
+        return uniqueName;
+    }
+
+    public String toString() {
+        return id + ":" + uniqueName;
+    }
+}
