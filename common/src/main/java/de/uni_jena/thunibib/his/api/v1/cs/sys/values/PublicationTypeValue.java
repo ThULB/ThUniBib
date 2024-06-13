206a12a2dd78917b89f9b4109589107c74441575
Silvio Hermann <s.hermann@uni-jena.de>
2024-06-13 13:40:17 +0200

    #550 Map ubo genre to his:publicationType (defaults to 'Sonstiger Publikationstyp' at the moment)

--- common/src/main/java/de/uni_jena/thunibib/his/api/v1/cs/sys/values/PublicationTypeValue.java
+++ common/src/main/java/de/uni_jena/thunibib/his/api/v1/cs/sys/values/PublicationTypeValue.java
@@ -31,79 +31,43 @@ public class PublicationTypeValue {
         return id;
     }
 
-    public void setId(int id) {
-        this.id = id;
-    }
-
     public int getLockVersion() {
         return lockVersion;
-    }
-
-    public void setLockVersion(int lockVersion) {
-        this.lockVersion = lockVersion;
     }
 
     public String getObjGuid() {
         return objGuid;
     }
 
-    public void setObjGuid(String objGuid) {
-        this.objGuid = objGuid;
-    }
-
     public String getShortText() {
         return shortText;
-    }
-
-    public void setShortText(String shortText) {
-        this.shortText = shortText;
     }
 
     public String getDefaultText() {
         return defaultText;
     }
 
-    public void setDefaultText(String defaultText) {
-        this.defaultText = defaultText;
-    }
-
     public String getLongText() {
         return longText;
-    }
-
-    public void setLongText(String longText) {
-        this.longText = longText;
     }
 
     public String getText() {
         return text;
     }
 
-    public void setText(String text) {
-        this.text = text;
-    }
-
     public int getHisKeyId() {
         return hisKeyId;
-    }
-
-    public void setHisKeyId(int hisKeyId) {
-        this.hisKeyId = hisKeyId;
     }
 
     public int getSortOrder() {
         return sortOrder;
     }
 
-    public void setSortOrder(int sortOrder) {
-        this.sortOrder = sortOrder;
-    }
-
     public String getUniqueName() {
         return uniqueName;
     }
 
-    public void setUniqueName(String uniqueName) {
-        this.uniqueName = uniqueName;
+    public String toString() {
+        return id + ":" + uniqueName;
     }
 }
