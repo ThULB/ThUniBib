362167f798af319f817b3bbe8a263cfe851b01eb
Silvio Hermann <s.hermann@uni-jena.de>
2024-06-14 12:49:23 +0200

    #550 Added publicationCreatorType

---
+++ common/src/main/java/de/uni_jena/thunibib/his/api/v1/cs/sys/values/PublicationCreatorTypeValue.java
@@ -0,0 +1,7 @@
+package de.uni_jena.thunibib.his.api.v1.cs.sys.values;
+
+/**
+ * Path: <code>/api/v1/cs/sys/values/publicationCreatorTypeValue</code>
+ * */
+public class PublicationCreatorTypeValue extends SysValue {
+}
