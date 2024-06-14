ac2d390e4174a62927996687f9e3f9ef047bc24c
Silvio Hermann <s.hermann@uni-jena.de>
2024-06-14 09:41:30 +0200

    #550 Add his/res status value as mods:classification

--- common/src/main/java/de/uni_jena/thunibib/his/api/HISInOneClient.java
+++ common/src/main/java/de/uni_jena/thunibib/his/api/HISInOneClient.java
@@ -7,7 +7,7 @@ import org.mycore.common.events.MCRShutdownHandler;
 public interface HISInOneClient extends MCRShutdownHandler.Closeable, AutoCloseable {
 
     String HIS_IN_ONE_BASE_URL = MCRConfiguration2.getStringOrThrow("ThUniBib.HISinOne.BaseURL");
-    String API_PATH = "api/v1/";
+    String API_PATH = MCRConfiguration2.getStringOrThrow("ThUniBib.HISinOne.BaseURL.API.Path");
 
     enum AuthType {
         Basic, Bearer
