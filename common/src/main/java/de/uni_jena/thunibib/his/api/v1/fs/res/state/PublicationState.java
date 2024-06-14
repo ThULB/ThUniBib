068589961ffd0db4e93c9d40663ee891a1ccaceb
Silvio Hermann <s.hermann@uni-jena.de>
2024-06-14 09:14:29 +0200

    #550 Added class VisibilityValue

--- common/src/main/java/de/uni_jena/thunibib/his/api/v1/fs/res/state/PublicationState.java
+++ common/src/main/java/de/uni_jena/thunibib/his/api/v1/fs/res/state/PublicationState.java
@@ -1,11 +1,11 @@
 package de.uni_jena.thunibib.his.api.v1.fs.res.state;
 
-import de.uni_jena.thunibib.his.api.v1.CommonType;
+import de.uni_jena.thunibib.his.api.v1.cs.sys.values.SysValue;
 
 /**
  * Path: <code>/api/v1/fs/res/state/publication</code>
  * */
-public class PublicationState extends CommonType {
+public class PublicationState extends SysValue {
     public PublicationState() {
     }
 }
