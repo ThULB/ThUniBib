4bd8daefb035fcd171f2dd7c7d30af6a33d3d497
Silvio Hermann <s.hermann@uni-jena.de>
2024-06-18 10:46:08 +0200

    #550 Map globalIdentifiers

---
+++ common/src/main/java/de/uni_jena/thunibib/his/api/v1/fs/res/publication/GlobalIdentifierType.java
@@ -0,0 +1,9 @@
+package de.uni_jena.thunibib.his.api.v1.fs.res.publication;
+
+import de.uni_jena.thunibib.his.api.v1.cs.sys.values.SysValue;
+
+/**
+ * Path: <code>fs/res/publication/globalIdentifierType</code>
+ * */
+public class GlobalIdentifierType extends SysValue {
+}
