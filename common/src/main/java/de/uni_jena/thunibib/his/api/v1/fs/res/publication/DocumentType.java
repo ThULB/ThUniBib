aa2cabdc9ed62b0ef31a1fe5a29ad7bf874b9fda
Silvio Hermann <s.hermann@uni-jena.de>
2024-06-18 09:14:52 +0200

    #550 Map documentType, defaults to Monograph

---
+++ common/src/main/java/de/uni_jena/thunibib/his/api/v1/fs/res/publication/DocumentType.java
@@ -0,0 +1,9 @@
+package de.uni_jena.thunibib.his.api.v1.fs.res.publication;
+
+import de.uni_jena.thunibib.his.api.v1.cs.sys.values.SysValue;
+
+/**
+ * Path: <code>fs/res/publication/documentTypes/&lt;book | article&gt;</code>
+ * */
+public class DocumentType extends SysValue {
+}
