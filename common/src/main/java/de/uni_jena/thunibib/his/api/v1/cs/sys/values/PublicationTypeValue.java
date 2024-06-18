c938860eec207c8e192819592413b67f4f696ebd
Silvio Hermann <s.hermann@uni-jena.de>
2024-06-18 14:05:47 +0200

    #550 Added static public String getPath() to classes extending SysValue

--- common/src/main/java/de/uni_jena/thunibib/his/api/v1/cs/sys/values/PublicationTypeValue.java
+++ common/src/main/java/de/uni_jena/thunibib/his/api/v1/cs/sys/values/PublicationTypeValue.java
@@ -4,6 +4,8 @@ package de.uni_jena.thunibib.his.api.v1.cs.sys.values;
  * Path: <code>/api/v1/cs/sys/values/publicationTypeValue</code>
  * */
 public class PublicationTypeValue extends SysValue {
-    public PublicationTypeValue() {
+
+    final static public String getPath() {
+        return "cs/sys/values/publicationTypeValue";
     }
 }
--- common/src/main/java/de/uni_jena/thunibib/his/api/v1/cs/sys/values/VisibilityValue.java
+++ common/src/main/java/de/uni_jena/thunibib/his/api/v1/cs/sys/values/VisibilityValue.java
@@ -4,4 +4,8 @@ package de.uni_jena.thunibib.his.api.v1.cs.sys.values;
  * Path: <code>/api/v1/cs/sys/values/visibilityValue</code>
  * */
 public class VisibilityValue extends SysValue {
+
+    final static public String getPath() {
+        return "cs/sys/values/visibilityValue";
+    }
 }
