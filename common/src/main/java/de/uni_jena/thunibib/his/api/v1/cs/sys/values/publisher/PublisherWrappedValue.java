6c327eaa8a1926a530bb3192bb20f22295f10e0a
Silvio Hermann <s.hermann@uni-jena.de>
2024-07-16 13:00:21 +0200

    #550 Resolve publisher

---
+++ common/src/main/java/de/uni_jena/thunibib/his/api/v1/cs/sys/values/publisher/PublisherWrappedValue.java
@@ -0,0 +1,44 @@
+package de.uni_jena.thunibib.his.api.v1.cs.sys.values.publisher;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import de.uni_jena.thunibib.his.api.v1.cs.sys.values.LanguageValue;
+import de.uni_jena.thunibib.his.api.v1.cs.sys.values.SysValue;
+
+/**
+ * Endpoint for <strong>searching</strong> publishers.
+ *
+ * PublisherWrappedValue
+ *
+ * Path: <code>/api/v1/fs/res/wrapped/publisher?q=</code>
+ * */
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class PublisherWrappedValue extends SysValue {
+    @JsonProperty("_foreigntext")
+    String _foreigntext;
+
+    @JsonProperty("place")
+    String place;
+
+    @JsonProperty("label")
+    String label;
+
+    @JsonProperty("language")
+    LanguageValue language;
+
+    public String getForeignText() {
+        return _foreigntext;
+    }
+
+    public String getPlace() {
+        return place;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public static String getPath() {
+        return "fs/res/wrapped/publisher";
+    }
+}
