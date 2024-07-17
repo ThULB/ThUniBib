ef549d299c4d635c3ed770316a24d53618a8662f
Silvio Hermann <s.hermann@uni-jena.de>
2024-07-17 13:03:36 +0200

    #550 Added class stub PublicationAndDocTypeMapper

---
+++ common/src/main/java/de/uni_jena/thunibib/his/xml/PublicationAndDocTypeMapper.java
@@ -0,0 +1,11 @@
+package de.uni_jena.thunibib.his.xml;
+
+public class PublicationAndDocTypeMapper {
+    public static String getPublicationTypeName(String ubogenreId) {
+        return "Monographie";
+    }
+
+    public static String getDocumentTypeName(String ubogenreId) {
+        return "Bibliographie";
+    }
+}
