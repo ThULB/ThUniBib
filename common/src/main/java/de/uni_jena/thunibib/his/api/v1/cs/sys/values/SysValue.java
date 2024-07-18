d653a1c7ea095811a89f271fe816e3ce696fab20
Silvio Hermann <s.hermann@uni-jena.de>
2024-07-18 13:52:54 +0200

    #550 Removed duplicates from documentType list

--- common/src/main/java/de/uni_jena/thunibib/his/api/v1/cs/sys/values/SysValue.java
+++ common/src/main/java/de/uni_jena/thunibib/his/api/v1/cs/sys/values/SysValue.java
@@ -66,7 +66,7 @@ abstract public class SysValue implements HisValue {
     }
 
     public String toString() {
-        return id + ":" + uniqueName;
+        return id + ":" + defaultText;
     }
 
     /**
