3e55280a381dba424ab59594084497dbdc1e9e28
Silvio Hermann <s.hermann@uni-jena.de>
2024-07-19 11:46:37 +0200

    #580 Added class HISInOneServiceFlag

---
+++ common/src/main/java/de/uni_jena/thunibib/his/xml/HISInOneServiceFlag.java
@@ -0,0 +1,15 @@
+package de.uni_jena.thunibib.his.xml;
+
+import de.uni_jena.thunibib.his.api.v1.fs.res.publication.Publication;
+
+import static de.uni_jena.thunibib.his.api.client.HISInOneClient.API_PATH;
+import static de.uni_jena.thunibib.his.api.client.HISInOneClient.HIS_IN_ONE_BASE_URL;
+
+public class HISInOneServiceFlag {
+    private HISInOneServiceFlag() {
+    }
+
+    public static String getName() {
+        return HIS_IN_ONE_BASE_URL + API_PATH + Publication.getPath();
+    }
+}
