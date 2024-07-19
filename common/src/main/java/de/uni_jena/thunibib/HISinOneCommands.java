6d363197b75932eba559ffb9c888d0dcb210ba5c
Silvio Hermann <s.hermann@uni-jena.de>
2024-07-19 10:27:37 +0200

    #580 Do not publish document twice in command "publish {0}"

--- common/src/main/java/de/uni_jena/thunibib/HISinOneCommands.java
+++ common/src/main/java/de/uni_jena/thunibib/HISinOneCommands.java
@@ -7,22 +7,27 @@ import jakarta.ws.rs.core.Response;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.jdom2.Document;
+import org.mycore.access.MCRAccessException;
 import org.mycore.common.content.MCRContent;
 import org.mycore.common.content.MCRJDOMContent;
 import org.mycore.common.content.transformer.MCRContentTransformer;
 import org.mycore.common.content.transformer.MCRContentTransformerFactory;
 import org.mycore.datamodel.metadata.MCRMetadataManager;
+import org.mycore.datamodel.metadata.MCRObject;
 import org.mycore.datamodel.metadata.MCRObjectID;
 import org.mycore.frontend.cli.annotation.MCRCommand;
 import org.mycore.frontend.cli.annotation.MCRCommandGroup;
 
 import java.io.IOException;
 
+import static de.uni_jena.thunibib.his.api.client.HISInOneClient.API_PATH;
 import static de.uni_jena.thunibib.his.api.client.HISInOneClient.HIS_IN_ONE_BASE_URL;
 
 @MCRCommandGroup(name = "HISinOne Commands")
 public class HISinOneCommands {
     private static final Logger LOGGER = LogManager.getLogger(HISinOneCommands.class);
+
+    private static final String FLAG_NAME = HIS_IN_ONE_BASE_URL + API_PATH + Publication.getPath();
 
     @MCRCommand(syntax = "publish {0}", help = "Publishes the object given by its id to HISinOne")
     public static void publish(String mcrid) {
@@ -38,19 +43,30 @@ public class HISinOneCommands {
             return;
         }
 
-        MCRContentTransformer transformer = MCRContentTransformerFactory.getTransformer("res-json-detailed");
-        Document mods = MCRMetadataManager.retrieveMCRObject(mcrObjectID).createXML();
+        MCRObject mcrObject = MCRMetadataManager.retrieveMCRObject(mcrObjectID);
+        if (mcrObject.getService().getFlags(FLAG_NAME).size() > 0) {
+            LOGGER.warn("{} is already published. Try command 'update {0}' instead?", mcrid);
+            return;
+        }
+
+        Document mods = mcrObject.createXML();
         try {
+            MCRContentTransformer transformer = MCRContentTransformerFactory.getTransformer("res-json-detailed");
             MCRContent transformed = transformer.transform(new MCRJDOMContent(mods));
             String json = transformed.asString();
             LOGGER.debug("JSON: {}", json);
+
             try (HISInOneClient client = HISinOneClientFactory.create();
                 Response response = client.post(Publication.getPath(), json)) {
                 Publication publication = response.readEntity(Publication.class);
                 LOGGER.info("MCRObject {} published at {} with id {}", mcrid, HIS_IN_ONE_BASE_URL, publication.getId());
+
+                //Update MCRObject
+                mcrObject.getService().addFlag(FLAG_NAME, String.valueOf(publication.getId()));
+                MCRMetadataManager.update(mcrObject);
             }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+        } catch (IOException | MCRAccessException e) {
+            LOGGER.error("Could not publish {} to {}", mcrid, HIS_IN_ONE_BASE_URL, e);
         }
     }
 
