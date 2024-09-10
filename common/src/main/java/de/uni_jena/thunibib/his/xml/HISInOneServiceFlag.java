34a140f4816c205cd27e3683f4253ae29740b991
Silvio Hermann <s.hermann@uni-jena.de>
2024-09-10 09:41:00 +0200

    #550 Made type of HISinOne servflag configurable

--- common/src/main/java/de/uni_jena/thunibib/his/xml/HISInOneServiceFlag.java
+++ common/src/main/java/de/uni_jena/thunibib/his/xml/HISInOneServiceFlag.java
@@ -1,10 +1,14 @@
 package de.uni_jena.thunibib.his.xml;
 
+import org.mycore.common.config.MCRConfiguration2;
+
 public class HISInOneServiceFlag {
+    static final String SERVFLAG_TYPE = MCRConfiguration2.getStringOrThrow("ThUniBib.HISinOne.servflag.type");
+
     private HISInOneServiceFlag() {
     }
 
     public static String getName() {
-        return "his-id";
+        return HISInOneServiceFlag.SERVFLAG_TYPE;
     }
 }
