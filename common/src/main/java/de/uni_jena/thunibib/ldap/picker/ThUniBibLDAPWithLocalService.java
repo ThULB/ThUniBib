295a5468a5844f648f7f123a8b3bcc5aab081fd2
Silvio Hermann <s.hermann@uni-jena.de>
2025-03-24 08:43:53 +0100

    FSU040THUL-4142 #604 Overwrite searchPerson from LDAPWithLocalService

--- common/src/main/java/de/uni_jena/thunibib/ldap/picker/ThUniBibLDAPWithLocalService.java
+++ common/src/main/java/de/uni_jena/thunibib/ldap/picker/ThUniBibLDAPWithLocalService.java
@@ -1,10 +1,13 @@
 package de.uni_jena.thunibib.ldap.picker;
 
 import org.mycore.ubo.ldap.picker.LDAPWithLocalService;
+import org.mycore.ubo.local.LocalService;
 import org.mycore.ubo.picker.IdentityService;
 import org.mycore.ubo.picker.PersonSearchResult;
+import org.mycore.user2.MCRRealmFactory;
 
 import javax.naming.OperationNotSupportedException;
+import java.util.Optional;
 
 /**
  * Class provided basically the same functionality as {@link LDAPWithLocalService} but unifies display of person names
@@ -17,12 +20,33 @@ public class ThUniBibLDAPWithLocalService extends LDAPWithLocalService {
     @Override
     public PersonSearchResult searchPerson(String query) throws OperationNotSupportedException {
         PersonSearchResult results = super.searchPerson(query);
-        flipNameParts(results.personList);
+
+        LocalService localService = new LocalService();
+        PersonSearchResult personSearchResult = localService.searchPerson(query);
+
+        super.flipNameParts(personSearchResult.personList);
+        results.join(personSearchResult, 0);
+
+        if (LDAPWithLocalService.LDAP_REALM != null) {
+            Optional.ofNullable(MCRRealmFactory.getRealm(LDAPWithLocalService.LDAP_REALM))
+                .ifPresent(
+                    realm -> results.join(localService.searchPerson(modifyQuery(query), realm), 0));
+        }
+
+        // flip name parts to unify display of names
+        super.flipNameParts(results.personList);
         return results;
     }
 
-    @Override
-    protected String modifyLocalQueryWithRealm(String query) {
+    /**
+     * Allow to modify the query when the {@link LocalService} is used for searching but a realm (different to
+     * {@link MCRRealmFactory#getLocalRealm()}) is provided. This is useful when the name format of the stored users
+     * by MyCoRe differs from that one provided by ldap.
+     *
+     * @param query
+     * @return
+     */
+    protected String modifyQuery(String query) {
         String[] parts = query.split("\\s* \\s*");
         if (parts.length == 2) {
             String firstName = parts[0];
