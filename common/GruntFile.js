c45d57e2e90753f154271209634a7b494343a74c
Silvio Hermann <s.hermann@uni-jena.de>
2025-05-16 12:04:48 +0200

    #710 Removed unused grunt task uglify from GruntFile.js

--- common/GruntFile.js
+++ common/GruntFile.js
@@ -1,6 +1,5 @@
 module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-npmcopy');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
     var path = require('path');
     var getAbsoluteDir = function (dir) {
         return path.isAbsolute(dir) ? dir : path.resolve(process.cwd(), dir);
