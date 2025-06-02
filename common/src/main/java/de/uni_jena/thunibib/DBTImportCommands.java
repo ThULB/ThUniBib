ce3d727e182637075d7e42cbdce17b4ecd251748
Silvio Hermann <s.hermann@uni-jena.de>
2025-06-02 10:56:14 +0200

    #730 Added support for querying DBT's solr core directly to avoid transmitting and parsing potentially large xml documents

--- common/src/main/java/de/uni_jena/thunibib/DBTImportCommands.java
+++ common/src/main/java/de/uni_jena/thunibib/DBTImportCommands.java
@@ -6,13 +6,11 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.solr.client.solrj.SolrQuery;
 import org.apache.solr.client.solrj.SolrServerException;
-import org.jdom2.Document;
+import org.apache.solr.client.solrj.impl.HttpSolrClient;
+import org.apache.solr.client.solrj.response.QueryResponse;
+import org.apache.solr.common.SolrDocument;
+import org.apache.solr.common.SolrDocumentList;
 import org.jdom2.Element;
-import org.jdom2.JDOMException;
-import org.jdom2.Text;
-import org.jdom2.filter.Filters;
-import org.jdom2.input.SAXBuilder;
-import org.jdom2.xpath.XPathExpression;
 import org.mycore.common.MCRMailer;
 import org.mycore.common.config.MCRConfiguration2;
 import org.mycore.frontend.MCRFrontendUtil;
@@ -20,13 +18,11 @@ import org.mycore.frontend.cli.annotation.MCRCommand;
 import org.mycore.frontend.cli.annotation.MCRCommandGroup;
 import org.mycore.services.i18n.MCRTranslation;
 import org.mycore.solr.MCRSolrClientFactory;
+import org.mycore.solr.MCRSolrCore;
 import org.mycore.ubo.importer.ImportJob;
 import org.mycore.ubo.importer.ListImportJob;
 
-import java.io.BufferedInputStream;
 import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
@@ -34,63 +30,94 @@ import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
-import static org.mycore.common.MCRConstants.XPATH_FACTORY;
-
 @MCRCommandGroup(name = "ThUniBib DBT-Import")
 public class DBTImportCommands {
     private static final Logger LOGGER = LogManager.getLogger(DBTImportCommands.class);
 
     protected static final String DBT_BASEURL = MCRConfiguration2.getStringOrThrow("ThUniBib.Importer.DBT.BaseURL");
 
-    @MCRCommand(syntax = "thunibib import from dbt by solr query {0}",
+    protected static final HttpSolrClient DBT_SOLR_CLIENT;
+
+    protected static final MCRSolrCore DBT_SOLR_CORE = MCRSolrClientFactory.get("dbt").orElseThrow();
+
+    static {
+        DBT_SOLR_CLIENT = DBT_SOLR_CORE.getClient();
+    }
+
+    @MCRCommand(syntax = "query dbt solr {0}", help = "Returns the number of hits for the given solr query (DBT-core)")
+    public static void checkDBTSolrReachable(String query) throws Exception {
+        QueryResponse response = DBT_SOLR_CLIENT.query(new SolrQuery(query));
+        LOGGER.info("{} hit(s) for query '{}' to DBT's solr core at '{}'", response.getResults().getNumFound(), query,
+            DBT_SOLR_CORE.getServerURL());
+    }
+
+    @MCRCommand(syntax = "import from dbt by solr query {0}",
         help = "Import new objects from DBT by the given solr query")
     public static List<String> importFromDBTByQuery(String solrQuery) {
         LOGGER.info("Importing from {} with solr query '{}'", DBT_BASEURL, solrQuery);
-        URL url;
 
+        SolrDocumentList solrDocuments = null;
         try {
-            String queryString = toQueryString(solrQuery);
-            url = new URL(DBT_BASEURL + "servlets/solr/select?" + queryString);
-        } catch (MalformedURLException e) {
-            LOGGER.error("Could not build url from {} and {}", DBT_BASEURL, solrQuery, e);
+            SolrQuery query = new SolrQuery(solrQuery);
+            query.setRows(Integer.MAX_VALUE);
+            query.setFields("id", "mods.identifier");
+            query.setFacet(false);
+
+            solrDocuments = DBT_SOLR_CLIENT.query(query).getResults();
+        } catch (SolrServerException | IOException e) {
+            LOGGER.error("Could not get solr documents for query '{}'", solrQuery);
             return null;
         }
 
-        Document solrDocument = getSolrDocument(url);
-        if (solrDocument == null) {
-            LOGGER.warn("Could not get solr document from {}", url);
-            return null;
-        }
-
-        List<String> identifiers = getIdentifiers(solrDocument);
+        List<String> identifiers = getIdentifiers(solrDocuments);
         if (identifiers.isEmpty()) {
             LOGGER.info("No new identifiers could be found");
             return null;
         }
 
-        String eligable = identifiers.stream().collect(Collectors.joining(" "));
-        LOGGER.info("The following {} DBT identifiers will be considered for import: {}", identifiers.size(), eligable);
-        return Arrays.asList("thunibib import " + eligable + " from dbt");
+        String eligible = identifiers.stream().collect(Collectors.joining(" "));
+        LOGGER.info("The following {} DBT identifiers will be considered for import: {}", identifiers.size(), eligible);
+        return Arrays.asList("import " + eligible + " from dbt");
     }
 
-    private static List<String> getIdentifiers(Document solrDocument) {
-        XPathExpression<Text> text = XPATH_FACTORY.compile("str[@name='id']/text()", Filters.text());
-
-        return XPATH_FACTORY.compile("//doc[str[@name='id']]", Filters.element())
-            .evaluate(solrDocument)
-            .stream()
+    /**
+     * Extracts the identifiers from Solr documents that have not been published in ThUniBib yet.
+     *
+     * @param solrDocumentList A {@link SolrDocumentList} containing the Solr documents to be processed.
+     * @return A {@code List} of String objects representing the identifiers of the documents
+     *         that have not been published. The list will be empty if all documents are already
+     *         published or if the input list contains no documents.
+     */
+    private static List<String> getIdentifiers(SolrDocumentList solrDocumentList) {
+        return solrDocumentList.stream()
             .filter(doc -> !publicationExists(doc))
-            .map(doc -> text.evaluateFirst(doc))
+            .map(doc -> doc.get("id"))
             .filter(Objects::nonNull)
-            .map(Text::getText)
+            .map(Object::toString)
             .collect(Collectors.toList());
     }
 
-    private static boolean publicationExists(Element doc) {
-        return XPATH_FACTORY.compile("//arr[@name='mods.identifier']/str/text()", Filters.text())
-            .evaluate(doc)
+    /**
+     * Checks if a publication already exists in the database.
+     *
+     * This method queries the Solr server to determine whether a given SolrDocument
+     * contains any identifiers ('mods.identifier') that match an existing publication's
+     * identifier in the Solr index. It performs a Solr query for each identifier found
+     * in the 'mods.identifier' field of the document and checks if there are any matching
+     * records in the Solr index.
+     *
+     * @param doc The SolrDocument to check for existing identifiers.
+     * @return true if at least one identifier from the document matches an existing publication's identifier
+     *         in the Solr index, otherwise false.
+     *
+     * <p>Note: In case of any SolrServerException or IOException during the query execution,
+     * the method logs the error and treats the identifier as existing to prevent potential
+     * duplicates. This conservative approach ensures data integrity at the expense of possibly
+     * ignoring new publications that failed to be queried successfully.</p>
+     */
+    private static boolean publicationExists(SolrDocument doc) {
+        return doc.getFieldValues("mods.identifier")
             .stream()
-            .map(Text::getText)
             .anyMatch(identifier -> {
                 try {
                     return MCRSolrClientFactory
@@ -104,35 +131,7 @@ public class DBTImportCommands {
             });
     }
 
-    private static String toQueryString(String solrQuery) {
-        StringBuilder b = new StringBuilder();
-
-        String[] nameValues = solrQuery.split("&");
-        for (int i = 0; i < nameValues.length; i++) {
-            String[] split = nameValues[i].split("=");
-            if (split.length == 2) {
-                b.append(
-                    (b.length() > 0 ? "&" : "") + split[0] + "=" + URLEncoder.encode(split[1], StandardCharsets.UTF_8));
-            }
-        }
-
-        return b.toString();
-    }
-
-    private static Document getSolrDocument(URL url) {
-        LOGGER.info("Getting solr document from '{}'", url);
-        try (BufferedInputStream is = new BufferedInputStream(url.openStream())) {
-            SAXBuilder builder = new SAXBuilder();
-            return builder.build(is);
-        } catch (IOException e) {
-            LOGGER.error("Could not open stream for url {}", url, e);
-        } catch (JDOMException e) {
-            LOGGER.error("Could not build xml", e);
-        }
-        return null;
-    }
-
-    @MCRCommand(syntax = "thunibib import {0} from dbt",
+    @MCRCommand(syntax = "import {0} from dbt",
         help = "Imports the objects from the DBT using their identifiers. Multiple identifiers can be specified separated by blank characters.")
     public static void importFromDBTByIdentifierList(String dbtid) {
         LOGGER.info("Importing DBT object {} from dbt", dbtid);
